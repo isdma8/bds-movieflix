66c1304833056072100f93f93425b0c51da23b32
Daniel Almeida <daniel.mag.almeida@gmail.com>
2020-11-19 16:31:04 +0000

    Backend MovieFlix Project Created

---
+++ backend/src/main/java/com/isdma/movieflixbds/MovieflixbdsApplication.java
@@ -0,0 +1,13 @@
+package com.isdma.movieflixbds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieflixbdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieflixbdsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/isdma/movieflixbds/MovieflixbdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isdma.movieflixbds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieflixbdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
