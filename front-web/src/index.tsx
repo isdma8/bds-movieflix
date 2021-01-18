e664c48a402a0cc4a0ba67dbc4f9b404dd9b7e5b
Daniel Almeida <daniel.mag.almeida@gmail.com>
2021-01-18 21:35:06 +0000

    Clean Project to begin

--- front-web/src/index.tsx
+++ front-web/src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
+
   document.getElementById('root')
 );
 
