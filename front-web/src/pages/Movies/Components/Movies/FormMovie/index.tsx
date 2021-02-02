8abf532f3e606580ff929dccec61c0a875a25f3a
Daniel Almeida <daniel.mag.almeida@gmail.com>
2021-02-02 17:22:23 +0000

    Movie Details Page Continue

--- front-web/src/pages/Movies/Components/Movies/FormMovie/index.tsx
+++ front-web/src/pages/Movies/Components/Movies/FormMovie/index.tsx
@@ -9,18 +9,21 @@ const FormMovie = () => {
     return (
         <MovieDetails >
             <div className="col-6">
-                <ImageTest />
+
+                <img src="https://image.tmdb.org/t/p/w220_and_h330_face/u3bZgnGQ9T01sWNhyveQz0wH0Hl.jpg" alt="test" className="image-card-details"/>
             </div>
             <div className="col-6">
-                <div className="movie-card-container">
+                <div className="movie-card-detail">
                     <h4>O Retorno do Rei</h4>
                     <h5>2013</h5>
                     <p>O olho do inimigo está se movendo</p>
-                    <p>O confronto final entre as forças do bem e do mal que lutam pelo controle do futuro da Terra Média se aproxima. 
+                    <div className="card-detail-description">
+                        O confronto final entre as forças do bem e do mal que lutam pelo controle do futuro da Terra Média se aproxima. 
                         Sauron planeja um grande ataque a Minas Tirith, capital de Gondor, o que faz com que Gandalf e Pippin partam para 
                         o local na intenção de ajudar a resistência. Um exército é reunido por Theoden em Rohan, em mais uma tentativa de 
                         deter as forças de Sauron. Enquanto isso, Frodo, Sam e Gollum seguem sua viagem rumo à Montanha da Perdição para destruir o anel.
-                    </p>
+                    
+                    </div>
 
                 </div>
             </div>
